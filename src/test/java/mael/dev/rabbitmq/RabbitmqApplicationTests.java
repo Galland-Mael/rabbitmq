625352489120f1ba3e8aae9dd8fd5aa1b9013038
mael <maelgalland.71@gmail.com>
2025-03-13 12:18:44 +0100

    Initial commit

---
+++ src/test/java/mael/dev/rabbitmq/RabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package mael.dev.rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
